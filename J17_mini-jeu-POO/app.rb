8e16afbe28d649b0feb18afc7133d29f392f0ae4
Célia <73950039+sweettsubaki@users.noreply.github.com>
2021-04-27 23:27:24 +0200

    Update app.rb

--- J17_mini-jeu-POO/app.rb
+++ J17_mini-jeu-POO/app.rb
@@ -5,8 +5,19 @@ require_relative 'lib/game'
 require_relative 'lib/player'
 
 
-    player1 = Player.new('Josiane') # create a new player
-    player2 = Player.new('José') ## create a new player
+player1 = Player.new('Josiane') # creation de nouveaux joueurs
+player2 = Player.new('José') 
     
+
+while player1.life_points > 0 && player2.life_points > 0
+  puts "Voici l'état de chaque joueur :"
+  puts player1.show_state
+  puts player2.show_state
+
+  puts "Passons à la phase d'attaque :"
+  player1.attacks(player2)
+  break if player2.life_points <= 0
+  player2.attacks(player1)
+end
 
 binding.pry
